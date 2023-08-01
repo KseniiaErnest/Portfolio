407470ccfbc131c99f4f4b2771763781fae95259
Kseniia Ernest <hramtcovakseniia@gmail.com>
2023-08-01 18:27:06 -0400

    Need to check responsivness, add pics of projects, one more project

--- script.js
+++ script.js
@@ -23,3 +23,23 @@ if(ent.isIntersecting) {
   rootMargin: '-64px'
 });
 observer.observe(sectionHero);
+
+// Infinite running text with GSAP
+const textContainer = document.querySelector('.contact-me-container');
+const textLine = document.querySelector('.contact-me-link');
+
+// Calculate the total width of the text line
+const textLineWidth = textLine.offsetWidth;
+
+// Set the initial position of the link at the beginning of the container
+gsap.set(textLine, { x: 0 });
+
+// Create the animation timeline
+const timeline = gsap.timeline({ repeat: -1, repeatDelay: 1 });
+
+// Add the animation to the timeline
+timeline.fromTo(
+  textLine,
+  { x: textContainer.offsetWidth },
+  { x: -textLineWidth, duration: 7, ease: 'linear' }
+);
